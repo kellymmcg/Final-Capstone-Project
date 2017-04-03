af5345a9edf1e0a14870bcd5c7df3d79319dfe09
Kelly McGowan <kellymmcgowan@gmail.com>
2017-04-03 13:56:10 -0400

    Go team bravo

---
+++ src/main/java/com/techelevator/city/model/UserDAO.java
@@ -0,0 +1,9 @@
+package com.techelevator.city.model;
+
+public interface UserDAO {
+
+	public void saveUser(String userName, String password);
+	public boolean searchForUsernameAndPassword(String userName, String password);
+
+	public void updatePassword(String userName, String password);
+}
