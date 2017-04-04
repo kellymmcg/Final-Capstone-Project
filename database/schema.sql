9b906e9ef876c166fd08c6fb93b643f57df95033
Kevin Crumedy <kcrumedy2@gmail.com>
2017-04-04 09:48:33 -0400

    added landmarks database

--- database/schema.sql
+++ database/schema.sql
@@ -5,7 +5,9 @@
 BEGIN;
 
 DROP TABLE IF EXISTS app_user;
-DROP SEQUENCE IF EXISTS seq_app_user_id;
+DROP SEQUENCE IF EXISTS seq_app_user_id;	
+DROP TABLE IF EXISTS landmark;
+DROP SEQUENCE IF EXISTS seq_landmark_id;	
 
 CREATE SEQUENCE seq_app_user_id;
 
@@ -13,6 +15,22 @@ CREATE TABLE app_user (
 	id INTEGER PRIMARY KEY DEFAULT NEXTVAL('seq_app_user_id'),
 	user_name VARCHAR(64) NOT NULL,
 	password VARCHAR(128) NOT NULL
+);
+
+CREATE SEQUENCE seq_landmark_id;
+
+CREATE TABLE landmark (
+id INTEGER PRIMARY KEY DEFAULT NEXTVAL('seq_landmark_id'),
+reviewid INTEGER,
+name VARCHAR(128) NOT NULL,
+longitude FLOAT NOT NULL,
+latitude FLOAT NOT NULL,
+address VARCHAR(256) NOT NULL,
+website VARCHAR(256),
+open_time INTEGER,
+close_time INTEGER,
+phone VARCHAR (32) NOT NULL
+
 );
 
 INSERT INTO app_user(user_name, password) VALUES ('KevinC', 'BravoAdmin');
