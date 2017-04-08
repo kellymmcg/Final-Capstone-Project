86167f8348e93fb40fa97bfcd7095ffd1c32e166
Kelly McGowan <kellymmcgowan@gmail.com>
2017-04-08 17:01:28 -0400

    The homepage carousel is now working and I changed the background images we may want to do a little more styling for the background images

--- src/main/webapp/js/homePageSlideShow.js
+++ src/main/webapp/js/homePageSlideShow.js
@@ -1,38 +1,7 @@
-$(document).ready(function() {
-  $(".carousel").carousel({
-    interval: 3500
-  });
-  $(".carousel").on("slid", function() {
-    var to_slide;
-    to_slide = $(".carousel-item.active").attr("data-slide-no");
-    $(".myCarousel-target.active").removeClass("active");
-    $(".carousel-indicators [data-slide-to=" + to_slide + "]").addClass("active");
-  });
-  $(".myCarousel-target").on("click", function() {
-    $(this).preventDefault();
-    $(".carousel").carousel(parseInt($(this).attr("data-slide-to")));
-    $(".myCarousel-target.active").removeClass("active");
-    $(this).addClass("active");
-  });
-});
-/*
+
 $(document).ready(function(){
     // Activate Carousel
     $("#myCarousel").carousel();
-    
-    // Enable Carousel Indicators
-    $(".item1").click(function(){
-        $("#myCarousel").carousel(0);
-    });
-    $(".item2").click(function(){
-        $("#myCarousel").carousel(1);
-    });
-    $(".item3").click(function(){
-        $("#myCarousel").carousel(2);
-    });
-    $(".item4").click(function(){
-        $("#myCarousel").carousel(3);
-    });
     
     // Enable Carousel Controls
     $(".left").click(function(){
@@ -42,4 +11,4 @@ $(document).ready(function(){
         $("#myCarousel").carousel("next");
     });
 });
-*/
+
